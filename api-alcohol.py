3888e90a596ef4806ac6803e0836fd71dbc081ab
nacho <nacho.conso@gmail.com>
2020-02-29 16:54:49 -0300

    added cli

--- api-alcohol.py
+++ api-alcohol.py
@@ -1,18 +1,20 @@
 import requests
 
-
 API_URL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?{}={}'
 
 def drink_search(drink, method = 's', results=1):
 	# drink = input('Ingresa la bebida chorborra: \n')
-	results_len = results - 1 #for use in array index format
 	resp = requests.get(API_URL.format(method, drink.lower()))
 	resp = resp.json()
 	elem = resp['drinks']
 	avoid = ['strInstructionsDE', 'strDrinkThumb', 'idDrink']
 	drinks_found = [{k:v for k,v in i.items() if k not in avoid and v} for i in elem]
-	print(drinks_found[results_len])
+	return drinks_found
 # print([k for k in primer_elem])
 # print([k for k in primer_elem.values()])
 
-drink_search('tequila')
+drink = input("Ingresa la bebida que estas buscando \n")
+num_de_resultados =int(input("cuantos resultados queres?\n"))
+response = drink_search(drink, results=num_de_resultados)
+for i in range(num_de_resultados):
+	print(response[i])
